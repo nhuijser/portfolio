a7f61718d3c71d8bfaab016640071ffa7c99a445
nhuijser <144018979+nhuijser@users.noreply.github.com>
2023-12-12 08:58:28 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+</head>
+<body>
+<div id="background"></div>
+    <title>nathan | portfolio</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <div id="particles-js"></div>
+
+    <!-- particles -->
+    <script>
+        particlesJS.load('particles-js', './particles/particles.json', function() {
+            console.log('particles.json loaded...');
+        });
+    </script>
+  <!-- particles -->
+<div class="dark-mode-button">
+<i id="moon-icon" class="fa-solid fa-moon fa-2xl" style="color: #ffffff;"></i>
+</div>
+<div class="header">
+  <h1><strong>nathan</strong></h1>
+  <p class="type"><strong>Software Developer Student</strong></p>
+  <p class="description">I'm passionate about building software that makes a difference in people's lives.</p>
+</div>
+
+<div class="nav">
+<strong>
+<ul>
+  <li><p>━ About me</p></li>
+  <li><p>━ Projects</p></li>
+  <li><p>━ Contact</p></li>
+</ul>
+</strong>
+</div>
+
+<div class="right-section">
+  <div class="section" id="about">
+    <p class="about-me">
+    When I was younger I was really into computers, how they work, operate etc. As I grew up I started learning how to write Discord bots using JavaScript. 
+    I then started learning how to make websites using HTML, CSS and JavaScript.
+    <br><br>
+    I am now studying Software Development at college and I am learning how to be a full-stack developer.
+    <br><br>
+    In my free time I mostly go to the gym, play video games and code.
+    </p>
+  </div>
+
+  <div class="section" id="experience">
+  <div class="timeline-div">
+                <p class="time-line">2023 - Present </p>
+            </div>
+            <div class="section-content">
+                <h3 class="role">balls.com </h3>
+                <h4 class="title">Senior Developer</h4>
+                <h4 class="title">Developer</h4>
+                <p class="text-area">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae minima iure ipsum
+                    eligendi sapiente tempore corporis possimus. Esse, hic debitis? Similique blanditiis sunt delectus,
+                    deserunt eaque quisquam alias?</p>
+            </div>
+        </div>
+
+</div>
+<script src="./script.js"></script>
+</div>
+</body>
+</html>
+
+<script src="https://kit.fontawesome.com/0f6a8fd9b7.js" crossorigin="anonymous"></script>
---
+++ script.js
@@ -0,0 +1,5 @@
+
+  document.getElementById('moon-icon').addEventListener('click', function() {
+    document.getElementById('background').classList.toggle('dark-mode');
+  });
+
